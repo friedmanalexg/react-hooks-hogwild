7ee3e9e6256305aa6e2d9af1442538ae9429744e
Andrianna <andrianna.odell@gmail.com>
2022-07-21 10:09:57 -0400

    We got pictures!

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import Nav from "./Nav";
-
+import RenderPigTile from "./PigTile";
 import hogs from "../porkers_data";
 
 function App() {
+	const pigCards = hogs.map((hogObj) => {
+		return(
+	  		<RenderPigTile
+      			key={hogObj.name} 
+      			name={hogObj.name} 
+      			image={hogObj.image} 
+      		/>
+		)
+	});
+
 	return (
 		<div className="App">
 			<Nav />
+			<main>
+			{pigCards}
+			</main>
 		</div>
 	);
 }
 
+// RenderPigTile(hogs.map);
 export default App;
