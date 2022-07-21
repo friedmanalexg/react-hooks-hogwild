8effa8b407062570d99cab2f8b7bf44d81531ede
Andrianna <andrianna.odell@gmail.com>
2022-07-21 11:07:33 -0400

    stuff was done

--- src/components/PigTile.js
+++ src/components/PigTile.js
@@ -3,9 +3,10 @@ import PigStats from "./PigStats";
 
 function RenderPigTile(props) {
       return(
-        <div className = "pigTile" onClick={PigStats}>
+        <div className = "pigTile">
             <h2>{props.name}</h2>
             <img src = {props.image} alt={props.name}/>
+            <PigStats />
         </div>
       );
 
