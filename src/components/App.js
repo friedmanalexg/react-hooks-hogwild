87e750ea43a305c00e0c4edd5f90ee57b57afe66
Andrianna <andrianna.odell@gmail.com>
2022-07-21 12:58:04 -0400

    started sort method

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,21 @@ import RenderPigTile from "./PigTile";
 import hogs from "../porkers_data";
 
 function App() {
+	// sort state
+	 const [sortStatus, setSortStatus] = useState(false);
+	 
+	//filter state
 	const [filterStatus, setFilterStatus] = useState(false);
 	const filteredHogs = hogs.filter(hog => hog.greased === filterStatus)
-	let pigCards = filteredHogs.map((hogObj) => {
+	const sortMethod = hogs.name;
+	const sortedPiggies = filteredHogs.sort(sortMethod)
+	// fix the sort method const to switch between booleans
+	
+
+	let pigCards = sortedPiggies.map((hogObj) => {
+
+		
+
 		return(
 	  		<RenderPigTile
       			key={hogObj.name} 
@@ -24,6 +36,7 @@ function App() {
 		<div className="App">
 			<Nav />
 			<button onClick={hideGreased}>Greased?</button>
+			<button onClick ={sortPiggies}>Sort By Weight or Name?</button>
 			<main>
 			{pigCards}
 			</main>
@@ -33,6 +46,12 @@ function App() {
 	function hideGreased() {
 		return setFilterStatus(prevFilterStatus => !prevFilterStatus);
 	}
+
+	 function sortPiggies() {
+	 	return setSortStatus(prevSortStatus => !prevSortStatus);
+	}
 }
+
+
 
 export default App;
